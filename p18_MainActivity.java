5281c70cfb7ac790b6874627f1b6e1a1717616b4
hardik3076 <hardikjadhav307@gmail.com>
2026-02-05 16:49:07 +0530

    Create p18_MainActivity.java

---
+++ p18_MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.intentdemo;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnOpenWebsite;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnOpenWebsite = findViewById(R.id.btnOpenWebsite);
+
+        btnOpenWebsite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Implicit Intent to open a website
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.google.com"));
+                startActivity(intent);
+            }
+        });
+    }
+}
