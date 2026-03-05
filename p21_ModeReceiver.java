aa5af4c9bba5c3127c33f265d83a36e407dbf128
hardik3076 <hardikjadhav307@gmail.com>
2026-03-05 15:29:34 +0530

    Add ModeReceiver to manage Airplane and audio modes
    Implement BroadcastReceiver to handle Airplane Mode changes and manage audio modes.

---
+++ p21_ModeReceiver.java
@@ -0,0 +1,61 @@
+package com.example.p21;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.widget.Toast;
+
+public class ModeReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        if (intent.getAction().equals(Intent.ACTION_AIRPLANE_MODE_CHANGED)) {
+
+            boolean isOn = intent.getBooleanExtra("state", false);
+
+            if (isOn) {
+                Toast.makeText(context, "Airplane Mode ON", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(context, "Airplane Mode OFF", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    // Function to set Silent Mode
+    public static void setSilent(Context context) {
+
+        AudioManager audioManager =
+                (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+
+        audioManager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+
+        Toast.makeText(context, "Silent Mode Activated", Toast.LENGTH_SHORT).show();
+    }
+
+    // Function to set Loud Mode
+    public static void setLoud(Context context) {
+
+        AudioManager audioManager =
+                (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+
+        audioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+
+        Toast.makeText(context, "Loud Mode Activated", Toast.LENGTH_SHORT).show();
+    }
+}
+/*
+app
+        ├── java
+        │    └── com.example.p21
+        │          ├── MainActivity.java
+        │          └── ModeReceiver.java
+        │
+        ├── res
+        │    └── layout
+        │          └── activity_main.xml
+        │
+        └── manifests
+        └── AndroidManifest.xml
+*/
