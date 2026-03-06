3dd7d937616f26736788735ff1252e8453875b47
hardik3076 <hardikjadhav307@gmail.com>
2026-03-06 13:44:47 +0530

    Update p22_DBHelper.java

--- p22_DBHelper.java
+++ p22_DBHelper.java
@@ -7,68 +7,71 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DBHelper extends SQLiteOpenHelper {
-public static final String DB_NAME="RegistrationDB";
-public static final String TABLE_NAME="Registration";
 
-public DBHelper(Context context) {
-    super(context, DB_NAME, null, 1);
-}
+    public static final String DB_NAME = "RegistrationDB";
+    public static final String TABLE_NAME = "Registration";
 
-@Override
-public void onCreate(SQLiteDatabase db) {
+    public DBHelper(Context context) {
+        super(context, DB_NAME, null, 1);
+    }
 
-    db.execSQL("CREATE TABLE Registration(id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT,email TEXT,phone TEXT,password TEXT)");
-}
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE Registration(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, email TEXT, phone TEXT, password TEXT)");
+    }
 
-@Override
-public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS Registration");
+        onCreate(db);
+    }
 
-    db.execSQL("DROP TABLE IF EXISTS Registration");
-    onCreate(db);
-}
+    public boolean insertData(String name, String email, String phone, String password) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
 
-public boolean insertData(String name,String email,String phone,String password){
+        cv.put("name", name);
+        cv.put("email", email);
+        cv.put("phone", phone);
+        cv.put("password", password);
 
-    SQLiteDatabase db=this.getWritableDatabase();
-    ContentValues cv=new ContentValues();
+        long result = db.insert(TABLE_NAME, null, cv);
+        db.close();
 
-    cv.put("name",name);
-    cv.put("email",email);
-    cv.put("phone",phone);
-    cv.put("password",password);
+        if (result == -1)
+            return false;
+        else
+            return true;
+    }
 
-    long result=db.insert(TABLE_NAME,null,cv);
+    public Cursor getData() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query(TABLE_NAME, null, null, null, null, null, null);
+        return cursor;
+    }
 
-    if(result==-1)
-        return false;
-    else
-        return true;
-}
+    public boolean updateData(String id, String name, String email, String phone, String password) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
 
-public Cursor getData(){
+        cv.put("name", name);
+        cv.put("email", email);
+        cv.put("phone", phone);
+        cv.put("password", password);
 
-    SQLiteDatabase db=this.getReadableDatabase();
-    Cursor cursor=db.rawQuery("SELECT * FROM Registration",null);
-    return cursor;
-}
+        long result = db.update(TABLE_NAME, cv, "id=?", new String[]{id});
+        db.close();
 
-public boolean updateData(String id,String name,String email,String phone,String password){
+        if (result == -1)
+            return false;
+        else
+            return true;
+    }
 
-    SQLiteDatabase db=this.getWritableDatabase();
-    ContentValues cv=new ContentValues();
-
-    cv.put("name",name);
-    cv.put("email",email);
-    cv.put("phone",phone);
-    cv.put("password",password);
-
-    db.update(TABLE_NAME,cv,"id=?",new String[]{id});
-    return true;
-}
-
-public Integer deleteData(String id){
-
-    SQLiteDatabase db=this.getWritableDatabase();
-    return db.delete(TABLE_NAME,"id=?",new String[]{id});
-}
+    public Integer deleteData(String id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        int result = db.delete(TABLE_NAME, "id=?", new String[]{id});
+        db.close();
+        return result;
+    }
 }
